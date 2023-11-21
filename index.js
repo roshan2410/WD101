1a72102d3cb3dfcbc356214925d22783d11f2611
roshan2410 <136004580+roshan2410@users.noreply.github.com>
2023-11-21 19:44:18 +0530

    index.js

--- index.js
+++ index.js
@@ -1,37 +1,79 @@
-document.getElementById('my-form').addEventListener('submit', function (event) {
+  document.addEventListener('DOMContentLoaded', function() {
+    // Load stored entries on page load
+    const storedEntries = JSON.parse(localStorage.getItem('registeredUsers')) || [];
+    displayEntries(storedEntries);
+  });
+
+  // Set the min and max attributes for the date input based on age range
+  const currentDate = new Date();
+  const minDate = new Date(currentDate);
+  minDate.setFullYear(minDate.getFullYear() - 55); // Min date (55 years ago)
+  const maxDate = new Date(currentDate);
+  maxDate.setFullYear(maxDate.getFullYear() - 18); // Max date (18 years ago)
+
+  const dobInput = document.getElementById('dob');
+  dobInput.setAttribute('max', maxDate.toISOString().split('T')[0]); // Set max date
+  dobInput.setAttribute('min', minDate.toISOString().split('T')[0]); // Set min date
+
+  document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    
+    // Get form values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dobInput = document.getElementById('dob').value;
+    const acceptedTerms = document.getElementById('terms').checked;
+    
+    // Date validation for age between 18 and 55
+    const currentDate = new Date();
+    const inputDate = new Date(dobInput);
+    const diff = currentDate - inputDate;
+    const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
 
-    const fullName = document.getElementById('fullName').value;
-    const userEmail = document.getElementById('userEmail').value;
-    const userPassword = document.getElementById('userPassword').value;
-    const userDob = document.getElementById('userDob').value;
-    const acceptTerms = document.getElementById('acceptTerms').checked;
+    const dobError = document.getElementById('dobError');
+    if (age < 18 || age > 55) {
+      dobError.textContent = 'Age should be between 18 and 55.';
+      return;
+    } else {
+      dobError.textContent = '';
+    }
 
-    const userDetails = {
-        fullName,
-        userEmail,
-        userPassword,
-        userDob,
-        acceptTerms
-    };
-    addEntryToTable(userDetails);
-    document.getElementById('my-form').reset();
-});
+    // Email validation
+    if (!validateEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
 
-function addEntryToTable(userDetails) {
-    const tableBody = document.getElementById('user-details');
-    const newRow = tableBody.insertRow();
+    // Add data to the table and local storage
+    const user = { name, email, password, dobInput, acceptedTerms };
+    let storedEntries = JSON.parse(localStorage.getItem('registeredUsers')) || [];
+    storedEntries.push(user);
+    localStorage.setItem('registeredUsers', JSON.stringify(storedEntries));
+    
+    // Display stored entries
+    displayEntries(storedEntries);
 
-    const cells = [
-        userDetails.fullName,
-        userDetails.userEmail,
-        userDetails.userPassword,
-        userDetails.userDob,
-        userDetails.acceptTerms ? 'Yes' : 'No'
-    ];
+    // Reset form
+    document.getElementById('registrationForm').reset();
+  });
 
-    cells.forEach((cellText) => {
-        const cell = newRow.insertCell();
-        cell.appendChild(document.createTextNode(cellText));
+  function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  function displayEntries(entries) {
+    const tableBody = document.getElementById('userTableBody');
+    tableBody.innerHTML = '';
+    
+    entries.forEach(entry => {
+      const row = tableBody.insertRow();
+      const rowData = [entry.name, entry.email, entry.password, entry.dobInput, entry.acceptedTerms ? 'true' : 'false'];
+
+      rowData.forEach((data, index) => {
+        const cell = row.insertCell(index);
+        cell.textContent = data;
+      });
     });
-}
+  }
